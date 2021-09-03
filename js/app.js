71bf6fa2928149e21376b8a8fe137a60b9972ca2
shamikdey <shamik7@gmail.com>
2021-09-04 03:06:24 +0600

    code fixed

--- js/app.js
+++ js/app.js
@@ -1,11 +1,12 @@
 const API_KEY = `6c4a135a0d8010abe87e9a8dd3da3a08`;
 const searchTemperature = () => {
     const city = document.getElementById('city-name').value;
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
 
     fetch(url)
         .then(res => res.json())
         .then(data => displayTemperature(data));
+
 }
 
 const setInnerText = (id, text) => {
@@ -13,6 +14,7 @@ const setInnerText = (id, text) => {
 }
 
 const displayTemperature = temperature => {
+    console.log(temperature);
     setInnerText('city', temperature.name);
     setInnerText('temperature', temperature.main.temp);
     setInnerText('condition', temperature.weather[0].main);
